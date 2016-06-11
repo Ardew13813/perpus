53462ea140bf531fba6828306e4d278446885aee
Ardhit <guax.raa@gmail.com>
2016-06-11 12:53:09 +0700

    try rev

---
+++ exercise 1/src/Greeting.java
@@ -0,0 +1,10 @@
+
+public class Greeting {
+	public String r = "dajshdkjahsd";
+	public String s = "sadasd";
+	public  void coba(){
+
+		System.out.println(r + s);
+		
+	}
+}
---
+++ exercise 1/src/TestGreetingJava.java
@@ -0,0 +1,9 @@
+
+public class TestGreetingJava {
+
+	public static void main(String[] a){
+
+		Greeting cetak = new Greeting();
+		cetak.coba();
+	}
+}
